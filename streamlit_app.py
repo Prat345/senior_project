77c81cc10e638c122e3a14a8d603fb01d81f468c
kopkap345 <123495257+kopkap345@users.noreply.github.com>
2023-03-11 17:02:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.set_page_config(page_title="Senior Project", page_icon="*", layout="wide")
 st.title("Data of Autonomous Vehicles Testdrive")
 st.markdown('#')
 
-with open(style.css') as f:
-          st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open('/style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # RETRIEVE DATA FROM DB
 connect = 'mongodb+srv://kopkap:kopkap123@cluster0.agjmc4n.mongodb.net/?retryWrites=true&w=majority' # Atlas
