25bbd1f973ffa624c953fc520c2c72175b601cb7
kopkap345 <123495257+kopkap345@users.noreply.github.com>
2023-03-15 00:49:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,7 +241,7 @@ def main():
     with q2:
         graph2(testdrive, df, incident_dict, map)
     st.markdown('#')
-    st.write(df[df['incident_flag'] != 0][['Time','msg.mode','msg.brake','linear_acceleration.x_filtered','elasped_time','incident_flag']])
+    st.write(df[df['incident_flag'] != 0])
     st.markdown('#')
     percent_mode(df)
 #-----------------------------------------------------------------------------------------
