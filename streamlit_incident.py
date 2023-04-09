060fd56d7533f63652a97bb86814b734af08a2b5
kopkap345 <123495257+kopkap345@users.noreply.github.com>
2023-04-09 17:55:56 +0700

    Update streamlit_incident.py

--- streamlit_incident.py
+++ streamlit_incident.py
@@ -7,6 +7,9 @@ import numpy as np
 import os
 import webbrowser
 import base64
+from collections import Counter
+from  itertools import chain
+from datetime import datetime
 
 # RETRIEVE DATA FROM DB
 connect = 'mongodb+srv://kopkap:kopkap123@cluster0.agjmc4n.mongodb.net/?retryWrites=true&w=majority' # Atlas
@@ -16,18 +19,57 @@ db = cluster["video"]
 db2 = cluster["Testdrive"]
 collection_list = db.list_collection_names()
 collection = db['incidents']
-
+#-------------------------------------------------------------------------------------------------
+def monthly(df):
+    plt.figure()
+    month = df
+    month['month'] = month['date'].astype('datetime64').apply(lambda x: x.strftime('%Y-%m'))
+    # st.write(month)
+    month2 = pd.Series(Counter(month['month'])).sort_index().rename_axis('month').reset_index(name='freq')
+    # st.write(month2)
+    # plt.barh(month2['month'], month2['freq'], color = 'navy', label = 'Total Mileages',align='center',height=0.5)
+    # st.pyplot(plt)
+    list_m = list(set(month['month']))
+    select_month= st.selectbox('Months',tuple(list_m))
+    gb = month.groupby(['month'])
+    df3 = gb.get_group(select_month)
+    # df3['tags'] = df3['tags'].str.strip('[]').str.replace("'",'').str.split(',') # Convert string to list
+    q1,q2 = st.columns(2)
+    with q1:
+        st.write(f'{len(df3)} incidents')
+        st.write(df3['tags'])
+    with q2:
+        barchart(df3)
+#-------------------------------------------------------------------------------------------------
+def barchart(df):
+    plt.figure()
+    df2 = pd.Series(Counter(chain(*df['tags']))).sort_index().rename_axis('tags').reset_index(name='freq')
+    # st.write(df2)
+    plt.barh(df2['tags'], df2['freq'], color = 'navy', label = 'Total Mileages',align='center',height=0.5)
+    plt.xlabel("amount") 
+    plt.ylabel("tags") 
+    plt.style.use('seaborn-whitegrid')
+    st.write('Existing Tags')
+    st.pyplot(plt)
+#-------------------------------------------------------------------------------------------------
+# STREAMLIT
+st.set_page_config(page_title="Senior Project", page_icon="*", layout="wide")
 st.header('Incident Search')
 st.markdown('#')
 df = collection.find()
 df = pd.DataFrame(df)
 df = df[df['tags'] != ""]
-st.write('Total incidents')
+df.index = np.arange(1, len(df) + 1)
+st.write(f'{len(df)} Total incidents')
 st.write(df.drop(columns = ['_id']))
 
+# st.markdown('#')
+# st.write('List of Tags')
+# st.image('Tags.png')
 st.markdown('#')
-st.write('List of Tags')
-st.image('Tags.png')
+barchart(df)
+monthly(df)
+st.markdown('#')
 st.write('Select Tags')
 factors = ['External','Internal']
 # EXTERNAL
@@ -53,12 +95,13 @@ def data_query():
     value = tags
     queries = collection.find({key:{"$all":value}})
     df_i = pd.DataFrame(queries)
+    df_i.index = np.arange(1, len(df_i) + 1)
     st.markdown('#')
     st.write('Results')
     st.write(df_i)
-    selected = st.selectbox('Select incident to view',tuple(list(np.arange(len(df_i)))))
+    selected = st.selectbox('Select incident to view',range(1,len(df_i)+1))-1
     path = df_i.iloc[selected ,df.columns.get_loc('path')]
-    URL = df_i.iloc[selected ,df.columns.get_loc('url')]
+    URL = df_i.iloc[selected,df.columns.get_loc('url')]
     testdrive = df_i.iloc[selected ,df.columns.get_loc('testdrive')]
     st.write('Video(drive)')
     st.write(URL)
@@ -81,3 +124,4 @@ def data_query():
     #-------------------------------------------------------------------------------------------------
 if tags !=[]:
     data_query()
+
